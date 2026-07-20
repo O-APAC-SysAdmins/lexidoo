26b7ddd8c609f5ef901bc32e7a7cb406be118164
4zv4l <4zv4l@protonmail.com>
2026-07-20 14:11:36 +0800

    up: add frawu + indent

--- systems/base-desktop.scm
+++ systems/base-desktop.scm
@@ -38,7 +38,7 @@
             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
         (channel
          (name 'guix)
-         (url "https://codeberg.org/guix/guix.git")
+         (url "https://codeberg.org/guix/guix")
          (branch "master")
          (introduction
           (make-channel-introduction
--- systems/lexidoo.scm
+++ systems/lexidoo.scm
@@ -24,6 +24,12 @@
                 (home-directory "/home/ande")
                 (supplementary-groups '("netdev" "audio" "video")))
                (user-account
+                (name "frawu")
+                (comment "Frances")
+                (group "users")
+                (home-directory "/home/frances")
+                (supplementary-groups '("netdev" "audio" "video")))
+               (user-account
                 (name "sibl")
                 (comment "Simon")
                 (group "users")
@@ -55,49 +61,52 @@
 
  (services
   (append (list 
-            (service rootless-podman-service-type
-			 (rootless-podman-configuration
-			  (subgids
-			   (list (subid-range (name "ande"))
-				     (subid-range (name "sibl"))))
-			  (subuids
-			   (list (subid-range (name "ande"))
-				     (subid-range (name "sibl"))))))
+           (service rootless-podman-service-type
+                    (rootless-podman-configuration
+                     (subgids
+                      (list (subid-range (name "ande"))
+                            (subid-range (name "frawu"))
+                            (subid-range (name "sibl"))))
+                     (subuids
+                      (list (subid-range (name "ande"))
+                            (subid-range (name "frawu"))
+                            (subid-range (name "sibl"))))))
 
-                (service openssh-service-type
-                         (openssh-configuration
-                           (port-number 2200)
-                           (permit-root-login #f)
-                           (x11-forwarding? #t)
-                           (password-authentication? #f)
-                           (public-key-authentication? #t)
-                           (authorized-keys
-                             `(("sibl" ,(plain-file "lexidoo_ssh_gpg" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLhOSwuzj0J74VPkb90Suky1roeMcj7Dt3J6Q9sU22GrWXPDCSIqKSzoXrYcNenRTKvJgw0qRIgTMWUUOCtBoXf8Q1AmbE/fzMa2EDQLtmylaOcOk7a6BhJmN2QjfwkDxb2t6BZSM7G1eOl2iOtWWY3F5QxbGU4locZYjeF16xOG+FOyDuvJldsJsAx/5lUcQvZfuwVACIpqxkto8Qab7qWLb4Qzj81fYtoGrTfdl5FuwAkVFsMlDLHSQzPmOrSMc4lRdcqyn5AqMl/hpzTKXP+1pqHOab5tykAO28//hG7I0n7JBC9J53CPrHFyUYLJznJQ4zIj3QjH4LvrCLVgaKI6rziuhurFFPDG47hchIXXysb4XWElMVD+nVu9y92nM9s6/Np47nsS1yn3jVmE2WLinGMk9z7sme4C05lepVW5sW77dG+hlv0OC4p0MPH1WN27LVwnYLWXQp0kElURMpBi4516RYNrQJ3hrdDlBXJj+pfocbTj5HvMqQph3S3OIazAqZ03dzlpzSwjVEU0eASjz6s/V0IvzMUEsJ+yHyCpimxUm1zRQ1hh6REnTCDLK4YPug/M9LHxK3aOzTFDNwZA1ot+NHjfiDmA26Myhrl74GjjRqT0AxLJ22kozVILtuf/0Ez6PpozzA4JLES7GZ9BngLPqWeAxrvuMLMJC5rw== sibl@sibl\n")
-                                        ,(plain-file "lexidoo_ssh" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8ue6dlphwDYWqNJhjmX9FzbvDjw+IGZd+hAlcBSAMs sibl@sibl\n"))
-                               ("ande" ,(plain-file "lexidoo_ssh" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUDDToHCKFCuj5JdknEdq+HI/J/Kk3ZivLXXqn5rSCr ande@odoo.com\n"))))))
+           (service openssh-service-type
+                    (openssh-configuration
+                     (port-number 2200)
+                     (permit-root-login #f)
+                     (x11-forwarding? #t)
+                     (password-authentication? #f)
+                     (public-key-authentication? #t)
+                     (authorized-keys
+                      `(("sibl" ,(plain-file "lexidoo_ssh_gpg" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLhOSwuzj0J74VPkb90Suky1roeMcj7Dt3J6Q9sU22GrWXPDCSIqKSzoXrYcNenRTKvJgw0qRIgTMWUUOCtBoXf8Q1AmbE/fzMa2EDQLtmylaOcOk7a6BhJmN2QjfwkDxb2t6BZSM7G1eOl2iOtWWY3F5QxbGU4locZYjeF16xOG+FOyDuvJldsJsAx/5lUcQvZfuwVACIpqxkto8Qab7qWLb4Qzj81fYtoGrTfdl5FuwAkVFsMlDLHSQzPmOrSMc4lRdcqyn5AqMl/hpzTKXP+1pqHOab5tykAO28//hG7I0n7JBC9J53CPrHFyUYLJznJQ4zIj3QjH4LvrCLVgaKI6rziuhurFFPDG47hchIXXysb4XWElMVD+nVu9y92nM9s6/Np47nsS1yn3jVmE2WLinGMk9z7sme4C05lepVW5sW77dG+hlv0OC4p0MPH1WN27LVwnYLWXQp0kElURMpBi4516RYNrQJ3hrdDlBXJj+pfocbTj5HvMqQph3S3OIazAqZ03dzlpzSwjVEU0eASjz6s/V0IvzMUEsJ+yHyCpimxUm1zRQ1hh6REnTCDLK4YPug/M9LHxK3aOzTFDNwZA1ot+NHjfiDmA26Myhrl74GjjRqT0AxLJ22kozVILtuf/0Ez6PpozzA4JLES7GZ9BngLPqWeAxrvuMLMJC5rw== sibl@sibl\n")
+                         ,(plain-file "lexidoo_ssh" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8ue6dlphwDYWqNJhjmX9FzbvDjw+IGZd+hAlcBSAMs sibl@sibl\n"))
+                        ("frawu" ,(plain-file "system_key" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOOVP4pOLhqCbSfaZbNxcSaGHpxPWzCM20agK8Tr9XX frawu@odoo.com\n"))
+                        ("ande" ,(plain-file "lexidoo_ssh" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUDDToHCKFCuj5JdknEdq+HI/J/Kk3ZivLXXqn5rSCr ande@odoo.com\n"))))))
 
-                (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout))))
+           (set-xorg-configuration
+            (xorg-configuration (keyboard-layout keyboard-layout))))
 
           %my-base-desktop-services))
 
  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
+              (bootloader grub-efi-bootloader)
+              (targets (list "/boot/efi"))
+              (keyboard-layout keyboard-layout)))
 
  (mapped-devices (list (mapped-device
-                         (source (uuid "90a2938c-12cd-441e-aacc-a077d120e104"))
-                         (target "cryptroot")
-                         (type luks-device-mapping))))
+                        (source (uuid "90a2938c-12cd-441e-aacc-a077d120e104"))
+                        (target "cryptroot")
+                        (type luks-device-mapping))))
 
  (file-systems (cons* (file-system
-                        (mount-point "/boot/efi")
-                        (device (uuid "C797-D8C5" 'fat32))
-                        (type "vfat"))
+                       (mount-point "/boot/efi")
+                       (device (uuid "C797-D8C5" 'fat32))
+                       (type "vfat"))
                       (file-system
-                        (mount-point "/")
-                        (device "/dev/mapper/cryptroot")
-                        (type "ext4")
-                        (dependencies mapped-devices))
+                       (mount-point "/")
+                       (device "/dev/mapper/cryptroot")
+                       (type "ext4")
+                       (dependencies mapped-devices))
                       %base-file-systems)))
